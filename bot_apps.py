ee1e72aaae5e325319f1751e8a6b0968de449432
NevoitGrigorii <109274244+NevoitGrigorii@users.noreply.github.com>
2025-09-14 18:58:19 +0300

    FIX: Serve index.html instead of text message

--- bot_apps.py
+++ bot_apps.py
@@ -47,13 +47,6 @@ def run_bot():
 # --- Flask частина для віддачі фронтенду та запуску бота ---
 app = Flask(__name__, static_folder='frontend', static_url_path='')
 
-
-@app.route('/')
-def home():
-    """Головна сторінка, яку буде перевіряти UptimeRobot."""
-    return "Bot is alive and running!"
-
-
 if __name__ == "__main__":
     # Запускаємо бота в окремому потоці, щоб він не блокував веб-сервер
     bot_thread = Thread(target=run_bot)
